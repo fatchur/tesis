a310f0f1cf3eaeba3fb573fc0225821ae409feff
fatchur.rahman <fatchur.rahman@prodiginow.com>
2025-05-30 06:22:50 +0700

    update: dropout rate to 0.10

--- training/config/config.py
+++ training/config/config.py
@@ -27,7 +27,7 @@ TRAINING_CONFIG = {
     'early_stopping_delta': 1e-4,
     'num_layers': 2,
     'hidden_size': 64,
-    'dropout_rate': 0.15,
+    'dropout_rate': 0.10,
     'leaky_relu_slope': 0.01,
     'activation': 'leaky_relu',
     'loss_alpha': 0.3,
